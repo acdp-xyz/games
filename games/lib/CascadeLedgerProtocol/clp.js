8a16a26e42e0543595de08ba223047683f4cb917
acdp-xyz <acdp.xyz@gmail.com>
2025-12-02 19:35:48 +0300

    Game Networking : Added Unique Hexademical Game ID Generation

---
+++ games/lib/CascadeLedgerProtocol/clp.js
@@ -0,0 +1,16 @@
+//GENERATE UNIQUE GAME ID
+let generateGameID = ()=>{
+    const randomNum1 = Math.floor(Math.random() * 16777216)
+    const randomNum2 = Math.floor(Math.random() * 16777216)
+    let hex1 = randomNum1.toString(16)
+    let hex2 = randomNum2.toString(16)
+    while(hex1.length < 6){
+        hex1 = "0" + hex1;
+    }
+    while(hex2.length < 6){
+        hex2 = "0" + hex2;
+    }
+    return hex1+hex2
+}
+ const gameID = generateGameID()
+ console.log(gameID)
